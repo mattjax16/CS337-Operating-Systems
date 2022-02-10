ef16011fe114a9d7d7d29e1cfe5de836ce717b87
mattjax16 <76124126+mattjax16@users.noreply.github.com>
2022-02-10 11:32:07 -0500

    Finished the FCFS scheduling Alg and added  operating_system.py

---
+++ Projects/Proj1/operating_system.py
@@ -0,0 +1,9 @@
+'''
+CS337 Spring 2022 - Operating Systems Prof. Al Madi
+operating_system.py
+Matthew Bass
+02/10/2022
+
+The operating_system.py file where processes are created, scheduler runs,
+and statistics are calculated.
+'''
--- Projects/Proj1/scheduler.py
+++ Projects/Proj1/scheduler.py
@@ -6,10 +6,32 @@
 # This is a file to the different process scheduling functions for the OS
 
 def FCFS_scheduler(processes, ready, CPU, time, debug=True):
-    ''' non-preemptive FCFS scheduler '''
+    ''' non-preemptive FCFS scheduler
+
+    Parameters:
+        processes: is a list of all the processes in the simulation,
+            whether they arrived or not, they are in this list.
+
+        ready: this is a list of processes with current arrival time.
+            Meaning, if the arrival time of a process is less than
+            the current time, it should be in the ready list.
+            Therefore, this list holds only processes that have
+            arrived at the ready list.
+
+        CPU: this is a list that simulates the CPU by holding beginning runtime
+            and end of runtime for each process. This is the same as the Gantt bar
+            that we have been using in lecture slides at the bottom of each example.
+
+        time: this is an integer that represents the current time, where simulation starts
+            at time zero and time is incremented by one after each time slice.
+
+        debug: this is a boolean with the default value of True. It controls a print
+            statement that shows process ID, start time, and end time at each context
+            switch. It is useful for debugging.
+    '''
 
     # pick process with lowest arrival time and remove it from ready
-    process = min(processes, lambda x:x.arrival_time)
+    process = min(ready, lambda x:x.arrival_time)
 
     # set start time to time
     start_time = time
@@ -22,8 +44,8 @@ def FCFS_scheduler(processes, ready, CPU, time, debug=True):
         # add 1 to time
         time += 1
 
-    # add processes that arrived now to ready queue
-    ready.push(process)
+        # add processes that arrived now to ready queue
+        add_ready(processes,ready,time)
 
     # set end time to time
     end_time = time
@@ -34,5 +56,29 @@ def FCFS_scheduler(processes, ready, CPU, time, debug=True):
                     Finish=end_time,
                     Priority=process.priority))
 
-    # return time
+    if debug:
+        print(f"Process ID: {process.id} , Start Time: {start_time} , End Time: {end_time}")
+
     return time
+
+
+def add_ready(processes,ready,time):
+    '''
+    Parameters:
+        processes: is a list of all the processes in the simulation,
+            whether they arrived or not, they are in this list.
+
+        ready: this is a list of processes with current arrival time.
+            Meaning, if the arrival time of a process is less than
+            the current time, it should be in the ready list.
+            Therefore, this list holds only processes that have
+            arrived at the ready list.
+
+
+        time: this is an integer that represents the current time, where simulation starts
+            at time zero and time is incremented by one after each time slice.
+    :param process:
+    :return:
+    '''
+    for i in range(len(processes)):
+        ready.push(processes.pop())
