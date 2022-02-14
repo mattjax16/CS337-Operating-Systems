949e7ecf08db7d16f22df00429260c97817980c8
Matt Bass <mnbass21@colby.edu>
2022-02-14 10:47:43 -0500

    added writing the CPU to csv

--- Projects/Proj1/operating_system.py
+++ Projects/Proj1/operating_system.py
@@ -8,15 +8,18 @@ The operating_system.py file where processes are created, scheduler runs,
 and statistics are calculated.
 '''
 
-#Importing the nessicary packages and objects
+# Importing the nessicary packages and objects
 import pandas as pd
 import scheduler
 from process import Process
+from datetime import datetime as dt
+
+
 # import numpy #using numpy to make more efficient
 
 
-def kernal(selected_scheduler,debug=False,
-           CPU_to_csv = True,Processes_to_csv = True):
+def kernal(selected_scheduler, debug=False,
+           CPU_to_csv=True, Processes_to_csv=True):
     """
      Simulates the CPU scheduling aspects of an operating system kernel.
 
@@ -27,44 +30,57 @@ def kernal(selected_scheduler,debug=False,
     :return:
     """
 
-    CPU = [] # A list to hold the data for some of the
-            # scheduled process for the CPU
+    CPU = []  # A list to hold the data for some of the
+    # scheduled process for the CPU
 
-    Scheduled_Processes = [] # A list to hold the scheduled process for the CPU
+    Scheduled_Processes = []  # A list to hold the scheduled process for the CPU
 
     ready = []  # A list to hold the process scheduled ready to be scheduled
 
     time = 0  # creating the intial time for the kernal
 
-    processes = [Process(0,5,0,30),Process(1,4,2,35),
-                 Process(2,1,5,36),Process(3,6,6,20)] # a list to hold all the processes to be schuedled for the CPU
-
+    processes = [Process(0, 5, 0, 30), Process(1, 4, 2, 35),
+                 Process(2, 1, 5, 36),
+                 Process(3, 6, 6, 20)]  # a list to hold all the processes to be schuedled for the CPU
 
     # adding the proccesses to the ready list
     # increment time until there is one
-    while(len(ready) == 0):
-        scheduler.add_ready(processes,ready,time)
+    while (len(ready) == 0):
+        scheduler.add_ready(processes, ready, time)
         if len(ready) == 0:
-            time +=1
+            time += 1
 
-
-
-    #runnig schuedler for all processes in ready
-    while(processes or ready):
-         time = selected_scheduler(processes, ready, CPU,Scheduled_Processes, time, debug=debug)
-
-
-
+    # runnig schuedler for all processes in ready
+    while (processes or ready):
+        time = selected_scheduler(processes, ready, CPU, Scheduled_Processes, time, debug=debug)
 
     # Once all the processes in the CPU that have finished
     # and calculate their wait time and turn around time
-    calc_wait_and_tunaround(CPU,Scheduled_Processes)
+    calc_wait_and_tunaround(CPU, Scheduled_Processes)
+
+    # Write data to csv files if either
+    # CPU_to_csv or Processes_to_csv is True
+    if CPU_to_csv or Processes_to_csv:
+
+        # get the current time for writing the file
+        time = dt.now().strftime(format="%y-%m-%d_%H-%M-%S")
+
+        # get the kind of scheduler used
+        if selected_scheduler == scheduler.FCFS_scheduler:
+            sched = "FCFS"
+
+
+        if CPU_to_csv:
+            #reverse CPU so it is written to df in process order
+            CPU.reverse()
+            pd.DataFrame(CPU).to_csv(f"data/CPU_{sched}_results_"+
+                                     f"{time}.csv",index=False)
 
 
     return
 
 
-def calc_wait_and_tunaround(CPU,Scheduled_Processes):
+def calc_wait_and_tunaround(CPU, Scheduled_Processes):
     '''
     Calculates the wait tiem and turn around for all the schuedled processes
 
@@ -75,27 +91,21 @@ def calc_wait_and_tunaround(CPU,Scheduled_Processes):
     :return:
     '''
 
-    #Sort the CPU and Schedled_process based on their id
-    CPU.sort(key = lambda x: x['process'],reverse = True)
+    # Sort the CPU and Schedled_process based on their id
+    CPU.sort(key=lambda x: x['process'], reverse=True)
     Scheduled_Processes.sort(key=lambda x: x.id, reverse=True)
 
-    for cpu_info , proc in zip(CPU,Scheduled_Processes):
+    for cpu_info, proc in zip(CPU, Scheduled_Processes):
         proc.wait_time = cpu_info['Start'] - proc.arrival_time
-        proc.turnaround_time =  cpu_info['Finish'] - proc.arrival_time
+        proc.turnaround_time = cpu_info['Finish'] - proc.arrival_time
 
     return
 
 
-
-
 # Main Testing function
 def main():
-
-    kernal(scheduler.FCFS_scheduler,True)
+    kernal(scheduler.FCFS_scheduler, True,CPU_to_csv=True)
 
 
 if __name__ == "__main__":
     main()
-
-
-
