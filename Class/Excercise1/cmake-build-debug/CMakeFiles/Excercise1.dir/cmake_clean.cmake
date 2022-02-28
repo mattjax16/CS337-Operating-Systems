9a62d83221513631c59c324412662137c6167d9b
Matt Bass <mnbass21@colby.edu>
2022-02-28 14:39:51 -0500

    added class excercise

---
+++ Class/Excercise1/cmake-build-debug/CMakeFiles/Excercise1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Excercise1.dir/library.c.o"
+  "libExcercise1.a"
+  "libExcercise1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Excercise1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Class/Excercise1/library.h
@@ -0,0 +1,6 @@
+#ifndef EXCERCISE1_LIBRARY_H
+#define EXCERCISE1_LIBRARY_H
+
+void hello(void);
+
+#endif //EXCERCISE1_LIBRARY_H
