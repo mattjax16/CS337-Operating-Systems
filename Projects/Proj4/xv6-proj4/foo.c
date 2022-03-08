fc8b194ce47ec942c76e490bf3c1861f7438aa7b
Matt Bass <mnbass21@colby.edu>
2022-03-07 22:38:33 -0500

    added foo.c for multiprocess testing

---
+++ Projects/Proj4/xv6-proj4/foo.c
@@ -0,0 +1,50 @@
+ /* 
+CS337 Spring 2022 - Operating Systems Prof. Al Madi
+Project 4
+foo.c
+Matthew Bass
+03/07/2022
+
+A program that uses fork to create multiple CPU-bound processes.  
+This will help us test our scheduler and system calls.
+*/
+ 
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+
+int main(int argc, char *argv[]) {
+  int pid;
+  int k, n;
+  int x, z;
+
+  if(argc < 2)
+        n = 1; //Default
+  else
+        n = atoi(argv[1]);
+  if (n < 0 ||n > 20)
+        n = 2;
+
+  printf(1, "creating %d processes!\n", n);
+  x = 0;
+  pid = 0;
+
+  for ( k = 0; k < n; k++ ) {
+    pid = fork ();
+    if ( pid < 0 ) {
+      printf(1, "%d failed in fork!\n", getpid());
+    } else if (pid > 0) {
+      // parent
+      printf(1, "Parent %d creating child %d\n",getpid(), pid);
+      wait();
+      }
+      else{
+        printf(1,"Child %d created\n",getpid());
+        for(z = 0; z < 4000000000; z+=1)
+            x = x + 3.14*89.64; //Useless calculation to consume CPU Time
+        break;
+      }
+  }
+  exit();
+}
